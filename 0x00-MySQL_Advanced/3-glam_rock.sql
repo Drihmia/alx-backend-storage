1d2ec10f3e39f1b4eb5871b7759a31d3a4918c16
Drihmia <drihmia.redouane@gmail.com>
2024-06-13 01:19:38 +0100

    1st commit in 0x00-MySQL_Advanced , task from 0 to 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,15 @@
+-- A SQL script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity.
+-- Column names will be: band_name and lifespan (in years until 2022 - 
+-- I'm only permited to use 2022 instead of YEAR(CURDATE())
+-- I shoud use attributes formed and split for computing the lifespan.
+-- My script can be executed on any database.
+SELECT
+    band_name,
+    CASE
+        WHEN split IS NULL THEN (2022 - formed)
+        ELSE (split - formed)
+    END AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
