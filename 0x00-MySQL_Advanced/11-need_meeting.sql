d621e180af10da2e9615a56e4524e725e1976424
Drihmia <drihmia.redouane@gmail.com>
2024-06-14 04:01:02 +0100

    [ADD] task11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -7,5 +7,5 @@ CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE
-    (score < 80 AND last_meeting IS NULL)
-    OR last_meeting > DATE_SUB(NOW(), INTERVAL 1 MONTH);
+    (score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH)));
+
