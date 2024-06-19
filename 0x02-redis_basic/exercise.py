11ad3af9714a094719ca14c64dee501c94e14aa2
Drihmia <drihmia.redouane@gmail.com>
2024-06-20 00:40:43 +0100

    [UPDATE] add response of task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,29 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    A decorator that ccreate input and output list keys, respectively.
+
+    example:
+    >>> lrange Cache.store:inputs 0 -1
+        1) "(b'first',)"
+        2) "(b'second',)"
+        3) "(b'third',)"
+    >>> lrange Cache.store:outputs 0 -1
+        1) "36e383a2-9f82-4d86-b005-c3c8f57b8ec2"
+        2) "7d73b286-c2b7-4c20-97e9-98309169ae0d"
+        3) "faef27a5-253a-4514-9258-afa2539fc77e"
+    """
+    @wraps(method)
+    def wrapper(self, *args):
+        self._redis.rpush(f"{method.__qualname__}:inputs", str(args[0:2]))
+        temp_ret = method(self, *args)
+        self._redis.rpush(f"{method.__qualname__}:outputs", temp_ret)
+        return temp_ret
+    return wrapper
+
+
 class Cache:
     """ The class cache """
 
@@ -30,6 +53,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         A method store the input data in Redis using the random key
@@ -45,6 +69,7 @@ class Cache:
         return str(random_key)
 
     @count_calls
+    @call_history
     def get(self, key: str, fn: Callable[[Any], T] = None) -> Optional[T]:
         """
         A method that gets back the data in Redis using provided key
@@ -59,12 +84,14 @@ class Cache:
         return returned_key
 
     @count_calls
+    @call_history
     def get_str(self, key: str) -> Optional[str]:
         """ Special case of the method: get """
 
         return self.get(key, fn=lambda d: d.decode("utf-8") if d else None)
 
     @count_calls
+    @call_history
     def get_int(self, key: str) -> Optional[int]:
         """ Special case of the method: get """
 
