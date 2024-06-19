10edc74309dc48a620489f176a7ca45d46cd9795
Drihmia <drihmia.redouane@gmail.com>
2024-06-19 23:17:01 +0100

    [UPDATE] add response of task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """ Task 0 """
 from redis import Redis
-from typing import Union
+from typing import Union, Callable, TypeVar, Any, Optional
 from uuid import uuid4
+
+
+T = TypeVar('T')
 
 
 class Cache:
@@ -24,6 +27,29 @@ class Cache:
         self._redis.set(str(random_key), data)
 
         # trigger the background save.
-        self._redis.bgsave()
+        # self._redis.bgsave()
 
         return str(random_key)
+
+    def get(self, key: str, fn: Callable[[Any], T]) -> Optional[T]:
+        """
+        A method that gets back the data in Redis using provided key
+
+        also it does convert the data from Redis using fn Callable
+
+        Return: data
+        """
+        returned_key = self._redis.get(key)
+        if fn is not None and returned_key:
+            returned_key = fn(returned_key)
+        return returned_key
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ Special case of the method: get """
+
+        return self.get(key, fn=lambda d: d.decode("utf-8") if d else None)
+
+    def get_int(self, key: str) -> Optional[int]:
+        """ Special case of the method: get """
+
+        return self.get(key, fn=int)
